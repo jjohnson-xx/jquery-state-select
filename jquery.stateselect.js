8c04bb659717cbc69a028cac0de829818b9ae2f5
jjohnson <josh@augustash.com>
2010-02-22 11:07:41 -0700

    Changed state/province values to the their 2-letter code and ordered them alphabetically

--- jquery.stateselect.js
+++ jquery.stateselect.js
@@ -7,95 +7,104 @@
  *  });
  *
  * Copyright (c) 2008 Adam Daniels
- * Licensed under the MIT License *
+ * Licensed under the MIT License
+ *
  */
 ;(function($) {
-  var canadian_provinces = {
-		'Ontario' : 'Ontario',
-		'Quebec' : 'Quebec',
-		'Manitoba' : 'Manitoba',
-		'Alberta' : 'Alberta',
-		'New Foundland and Labrador' : 'New Foundland and Labrador',
-		'Nova Scotia' : 'Nova Scotia',
-		'Prince Edward Island' : 'Prince Edward Island',
-		'Saskatchewan' : 'Saskatchewan',
-		'Nunavut' : 'Nunavut',
-		'British Columbia' : 'British Columbia',
-		'New Brunswick' : 'New Brunswick',
-		'Yukon' : 'Yukon',
-		'Northwest Territories' : 'Northwest Territories'
-	}
-
-	var us_states = {
-		'Alabama' : 'Alabama',
-		'Alaska' : 'Alaska',
-		'Arizona' : 'Arizona',
-		'Arkansas' : 'Arkansas',
-		'California' : 'California',
-		'Colorado' : 'Colorado',
-		'Connecticut' : 'Connecticut',
-		'Delaware' : 'Delaware',							
-		'Florida' : 'Florida',
-		'Georgia' : 'Georgia',
-		'Hawaii' : 'Hawaii',
-		'Idaho' : 'Idaho',
-		'Illinois' : 'Illinois',
-		'Indiana' : 'Indiana',
-		'Iowa' : 'Iowa',
-		'Kansas' : 'Kansas',
-		'Kentucky' : 'Kentucky',
-		'Louisiana' : 'Louisiana',
-		'Maine' : 'Maine',
-		'Maryland' : 'Maryland',
-		'Massachusetts' : 'Massachusetts',
-		'Michigan' : 'Michigan',
-		'Minnesota' : 'Minnesota',
-		'Mississippi' : 'Mississippi',
-		'Missouri' : 'Missouri',
-		'Montana' : 'Montana',
-		'Nebraska' : 'Nebraska',
-		'Nevada' : 'Nevada',
-		'New Hampshire' : 'New Hampshire',
-		'New Jersey' : 'New Jersey',							
-		'New Mexico' : 'New Mexico',
-		'New York' : 'New York',
-		'North Carolina' : 'North Carolina',
-		'North Dakota' : 'North Dakota',
-		'Ohio' : 'Ohio',
-		'Oklahoma' : 'Oklahoma',
-		'Oregon' : 'Oregon',
-		'Pennsylvania' : 'Pennsylvania',
-		'Rhode Island' : 'Rhode Island',
-		'South Carolina' : 'South Carolina',
-		'South Dakota' : 'South Dakota',
-		'Tennessee' : 'Tennessee',
-		'Texas' : 'Texas',
-		'Utah' : 'Utah',
-		'Vermont' : 'Vermont',
-		'Virginia' : 'Virginia',
-		'Washington' : 'Washington',
-		'West Virginia' : 'West Virginia',
-		'Wisconsin' : 'Wisconsin',
-		'Wyoming' : 'Wyoming'
-	}
-  
-  $.fn.extend({
-		linkToStates: function(state_select_id) {
-      $(this).change(function() {
-        var country = $(this).attr('value');
-        $(state_select_id).removeOption(/.*/);
-        switch (country) {
-          case 'Canada':
-            $(state_select_id).addOption(canadian_provinces, false);
-            break;
-          case 'United States':
-            $(state_select_id).addOption(us_states, false);
-            break;
-          default:
-            $(state_select_id).addOption({ '' : 'Please select a Country'}, false);
-            break;
-        }
-      });
+    var canadian_provinces = {
+        'AB' : 'Alberta',
+        'BC' : 'British Columbia',
+        'MB' : 'Manitoba',
+        'NB' : 'New Brunswick',
+        'NL' : 'New Foundland and Labrador',
+        'NT' : 'Northwest Territories',
+        'NS' : 'Nova Scotia',
+        'NU' : 'Nunavut',
+        'ON' : 'Ontario',
+        'PE' : 'Prince Edward Island',
+        'QC' : 'Quebec',
+        'SK' : 'Saskatchewan',
+        'YT' : 'Yukon'
     }
-  });
+    var us_states = {
+	'AL' : 'Alabama',
+        'AK' : 'Alaska',
+        'AS' : 'American Samoa',
+        'AZ' : 'Arizona',
+        'AR' : 'Arkansas',
+        'CA' : 'California',
+        'CO' : 'Colorado',
+        'CT' : 'Connecticut',
+        'DE' : 'Delaware',
+        'DC' : 'District Of Columbia',
+        'FM' : 'Federated States of Micronesia',
+        'FL' : 'Florida',
+        'GA' : 'Georgia',
+        'GU' : 'Guam',
+        'HI' : 'Hawaii',
+        'ID' : 'Idaho',
+        'IL' : 'Illinois',
+        'IN' : 'Indiana',
+        'IA' : 'Iowa',
+        'KS' : 'Kansas',
+        'KY' : 'Kentucky',
+        'LA' : 'Louisiana',
+        'ME' : 'Maine',
+        'MH' : 'Marshall Islands',
+        'MD' : 'Maryland',
+        'MA' : 'Massachusetts',
+        'MI' : 'Michigan',
+        'MN' : 'Minnesota',
+        'MS' : 'Mississippi',
+        'MO' : 'Missouri',
+        'MT' : 'Montana',
+        'NE' : 'Nebraska',
+        'NV' : 'Nevada',
+        'NH' : 'New Hampshire',
+        'NJ' : 'New Jersey',
+        'NM' : 'New Mexico',
+        'NY' : 'New York',
+        'NC' : 'North Carolina',
+        'ND' : 'North Dakota',
+        'MP' : 'Northern Mariana Islands',
+        'OH' : 'Ohio',
+        'OK' : 'Oklahoma',
+        'OR' : 'Oregon',
+        'PW' : 'Palau',
+        'PA' : 'Pennsylvania',
+        'PR' : 'Puerto Rico',
+        'RI' : 'Rhode Island',
+        'SC' : 'South Carolina',
+        'SD' : 'South Dakota',
+        'TN' : 'Tennessee',
+        'TX' : 'Texas',
+        'UT' : 'Utah',
+        'VT' : 'Vermont',
+        'VA' : 'Virginia',
+        'VI' : 'Virgin Islands'
+        'WA' : 'Washington',
+        'WV' : 'West Virginia',
+        'WI' : 'Wisconsin',
+        'WY' : 'Wyoming'
+    }
+  
+    $.fn.extend({
+        linkToStates: function(state_select_id) {
+            $(this).change(function() {
+                var country = $(this).attr('value');
+                $(state_select_id).removeOption(/.*/);
+                switch (country) {
+                    case 'Canada':
+                        $(state_select_id).addOption(canadian_provinces, false);
+                        break;
+                    case 'United States':
+                        $(state_select_id).addOption(us_states, false);
+                        break;
+                    default:
+                        $(state_select_id).addOption({ '' : 'Please select a Country'}, false);
+                        break;
+                }
+            });
+        }
+    });
 })(jQuery);
